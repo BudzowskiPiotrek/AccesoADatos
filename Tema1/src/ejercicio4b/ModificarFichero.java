f04ed3deefe1aa5c24fe4d47f6582177b57d2a5c
Piotrek Budzowski <piobudzows@gmail.com>
2025-09-24 18:38:17 +0200

    Modificado la tarea de acceso datos

--- Tema1/src/ejercicio4b/ModificarFichero.java
+++ Tema1/src/ejercicio4b/ModificarFichero.java
@@ -6,21 +6,34 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
-public class App {
+public class ModificarFichero {
 
 	public static void main(String[] args) {
+		// CREAO LAS RUTAS DEL FICHERO Y RUTA DEL FICHERO AUXILIAR
+		// EL FICHERO ESTA COMPUESTO POR DNI-APELLIDO-NOMBRE
 		String rutaFicheroEntrada = System.getProperty("user.home") + "\\Desktop\\fichero.txt";
-		String rutaFicheroSalida = System.getProperty("user.home") + "\\Desktop\\fichero_modificado.txt";
-				
+		String rutaFicheroSalida = System.getProperty("user.home") + "\\Desktop\\fichero_auxiliar.txt";
+		Scanner st = new Scanner(System.in);
+		// LE PREGUNTAMOS POR EL APELLIDO CUAL SE NODIFICARA
+		System.out.println("Dime el dni para poder modificar su nombre y apellido:");
+		String dni = st.nextLine();
+		System.out.println("Dime su nuevo Apellido correspondiente:");
+		String apellido = st.nextLine();
+		System.out.println("Dime su nuevo Nombre correspondiente:");
+		String nombre = st.nextLine();
+
+		// CREAMOS BR Y BW DENTRO DE TRY USANDO SU RECURSO
 		try (BufferedReader br = new BufferedReader(new FileReader(rutaFicheroEntrada));
 				BufferedWriter bw = new BufferedWriter(new FileWriter(rutaFicheroSalida))) {
-
 			String linea;
+			// RECOREMOS LINEA POR LINEA CONVIERTIENDOLA EN UNA ARRAY BUSCANDO CONCIDENCIA
 			while ((linea = br.readLine()) != null) {
 				String[] campos = linea.split(" ");
-				if (campos[0].equalsIgnoreCase("Piotrek")) {
-					campos[0] = "Carlos";
+				if (campos.length >= 3 && campos[0].equalsIgnoreCase(dni)) {
+					campos[1] = apellido;
+					campos[2] = nombre;
 					StringBuilder nuevaLinea = new StringBuilder();
 					for (int i = 0; i < campos.length; i++) {
 						nuevaLinea.append(campos[i]);
@@ -32,19 +45,17 @@ public class App {
 				} else {
 					bw.write(linea);
 				}
-				bw.newLine(); 
+				bw.newLine(); // \n
 			}
 		} catch (IOException e) {
-			System.err.println("Ocurrió un error al leer o escribir el fichero:");
 			e.printStackTrace();
 		}
-		
+		// ULTIMA FASE DE EJERCICIO BORAMOS EL PRIMERO Y SEGUNDO RENOMBRAMOS
 		File f1 = new File(rutaFicheroEntrada);
 		File f2 = new File(rutaFicheroSalida);
-		
+
 		f1.delete();
 		f2.renameTo(f1);
-		
-		
+		st.close();
 	}
 }
