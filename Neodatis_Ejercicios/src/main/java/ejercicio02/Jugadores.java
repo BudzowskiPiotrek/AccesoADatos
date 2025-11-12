365bcd3fe0448fc4a5a7e6908db10189568f28d9
Piotrek Budzowski <piobudzows@gmail.com>
2025-11-12 22:15:48 +0100

    ejercicio 2 de neodatis

---
+++ Neodatis_Ejercicios/src/main/java/ejercicio02/Jugadores.java
@@ -0,0 +1,35 @@
+package ejercicio02;
+
+public class Jugadores {
+	private String nombre;
+	private Paises pais;
+
+	public Jugadores() {
+	}
+
+	public Jugadores(String nombre, Paises pais) {
+		this.nombre = nombre;
+		this.pais = pais;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Paises getPais() {
+		return pais;
+	}
+
+	public void setPais(Paises pais) {
+		this.pais = pais;
+	}
+
+	@Override
+	public String toString() {
+		return "Jugador [Nombre: " + nombre + ", País: " + pais.getNombrepais() + "]";
+	}
+}
