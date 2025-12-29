b2a9950e9cd6f83a860cf7ab2bcce2ab5660184b
Piotrek Budzowski <piobudzows@gmail.com>
2025-12-29 23:52:16 +0100

    Otra practica xml

--- RepasoTotal/src/main/java/XML/PruebasEjercicios.java
+++ RepasoTotal/src/main/java/XML/PruebasEjercicios.java
@@ -20,11 +20,13 @@ import org.w3c.dom.Document;
 
 import XML.utils.Ejercicio1;
 import XML.utils.Ejercicio2;
+import XML.utils.Ejercicio3;
 
 public class PruebasEjercicios {
 	public static void main(String[] args) {
-		ejercicio1();
-		ejercicio2();
+		//ejercicio1();
+		//ejercicio2();
+		ejercicio3();
 	}
 
 	private static void ejercicio1() {
@@ -55,6 +57,7 @@ public class PruebasEjercicios {
 	}
 
 	private static void ejercicio2() {
+
 		Ejercicio2 e1 = new Ejercicio2();
 		try {
 			DocumentBuilderFactory dF = DocumentBuilderFactory.newInstance();
@@ -79,5 +82,34 @@ public class PruebasEjercicios {
 		} catch (TransformerException e) {
 			System.err.println("Detalle del error: " + e.getMessage());
 		}
+	}
+
+	private static void ejercicio3() {
+		Ejercicio3 e3 = new Ejercicio3();
+
+		try {
+			DocumentBuilderFactory dF = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dB = dF.newDocumentBuilder();
+			DOMImplementation im = dB.getDOMImplementation();
+			Document doc = im.createDocument(null, "universo", null);
+			doc.setXmlVersion("1.0");
+
+			doc = e3.crearDocumentoXML(doc, "MW-01");
+
+			Source s = new DOMSource(doc);
+			Result r = new StreamResult(new File("D:\\xml\\universo.xml"));
+			Transformer t = TransformerFactory.newInstance().newTransformer();
+			t.transform(s, r);
+
+		} catch (ParserConfigurationException e) {
+			System.err.println("Error al intentar crear documento : , detalle del error : " + e.getMessage());
+		} catch (TransformerConfigurationException e) {
+			System.err.println("Detalle del error: " + e.getMessage());
+		} catch (TransformerFactoryConfigurationError e) {
+			System.err.println("Detalle del error: " + e.getMessage());
+		} catch (TransformerException e) {
+			System.err.println("Detalle del error: " + e.getMessage());
+		}
+
 	}
 }
---
+++ RepasoTotal/src/main/java/XML/utils/Ejercicio3.java
@@ -0,0 +1,70 @@
+package XML.utils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class Ejercicio3 {
+	ConnectionSQL conSQL = new ConnectionSQL();
+
+	public Document crearDocumentoXML(Document doc, String condicion) {
+		String sql = "SELECT g.nombre as nom_g, g.tipo, e.nombre as nom_e, p.es_habitable, p.nombre as nom_p "
+				+ "FROM galaxias g left JOIN estrellas e ON g.id_galaxia=e.id_galaxia Left JOIN planetas p ON e.id_estrella=p.id_estrella WHERE g.id_galaxia = ? Order by e.nombre;";
+		Element listado = doc.createElement("listado_estrellas");
+
+		int control = 0;
+		String estrellaActual = "";
+		Element estrella = null;
+		Element planetas = null;
+
+		try (Connection con = conSQL.conectar("practica_astronomia");
+				PreparedStatement pr = con.prepareStatement(sql)) {
+
+			pr.setString(1, condicion);
+
+			try (ResultSet rs = pr.executeQuery()) {
+				while (rs.next()) {
+					if (control == 0) {
+						doc.getDocumentElement().setAttribute("galaxia", rs.getString("nom_g"));
+						doc.getDocumentElement().setAttribute("tipo", rs.getString("tipo"));
+						control++;
+					}
+
+					String nombreE = rs.getString("nom_e");
+
+					if (nombreE != null && !nombreE.equals(estrellaActual)) {
+						estrellaActual = nombreE;
+
+						estrella = doc.createElement("estrella");
+						estrella.setAttribute("nombre", nombreE);
+						planetas = doc.createElement("planetas");
+
+						estrella.appendChild(planetas);
+						listado.appendChild(estrella);
+					}
+
+					String nombreP = rs.getString("nom_p");
+
+					if (nombreP != null) {
+						Element planeta = doc.createElement("planeta");
+						planeta.setAttribute("habitable", rs.getInt("es_habitable") == 1 ? "si" : "no");
+						planeta.appendChild(doc.createTextNode(nombreP));
+						planetas.appendChild(planeta);
+					}
+				}
+
+				doc.getDocumentElement().appendChild(listado);
+			}
+		} catch (SQLException e) {
+			System.err.println("Error SQL: " + e.getMessage());
+		}
+		return doc;
+	}
+
+}
