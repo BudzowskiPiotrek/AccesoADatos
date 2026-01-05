e6ef959cb5ac7f9a9977f81e0c1e83c92df8747b
Piotrek Budzowski <piobudzows@gmail.com>
2026-01-05 21:37:01 +0100

    Update Ejercicio8.java

--- RepasoTotal/src/main/java/XML/utils/Ejercicio8.java
+++ RepasoTotal/src/main/java/XML/utils/Ejercicio8.java
@@ -1,5 +1,62 @@
 package XML.utils;
 
-public class Ejercicio8 {
+import java.sql.Date;
 
+import org.neodatis.odb.ODB;
+import org.neodatis.odb.ODBRuntimeException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import XML.models.Duenos;
+import XML.models.Pacientes;
+import XML.models.Visita;
+
+public class Ejercicio8 {
+	private ConnectionNeodatis conNeo = new ConnectionNeodatis();
+	private final String NOMBRE_BD = "clinica.odb";
+
+	public boolean CrearNeodatis(Document doc) {
+		ODB con = null;
+		try {
+			con = conNeo.abrir(NOMBRE_BD);
+
+			Element dueno = (Element) doc.getElementsByTagName("dueño").item(0);
+			Duenos d1 = new Duenos(dueno.getAttribute("nombre"));
+
+			NodeList listaMascotas = doc.getElementsByTagName("mascota");
+
+			for (int i = 0; i < listaMascotas.getLength(); i++) {
+				Element ma = (Element) listaMascotas.item(i);
+				String nombreMascota = ma.getAttribute("nombre");
+				String especie = ma.getAttribute("especie");
+				Pacientes p1 = new Pacientes(nombreMascota, especie);
+				d1.getListaMascotas().add(p1);
+
+				NodeList listaVisitas = ma.getElementsByTagName("visita");
+
+				for (int j = 0; j < listaVisitas.getLength(); j++) {
+					Element vi = (Element) listaVisitas.item(j);
+					Date fecha = Date.valueOf(vi.getAttribute("fecha"));
+					String diagnostico = vi.getTextContent();
+					double precio = Double.valueOf(vi.getAttribute("precio"));
+					p1.getListaVisitas().add(new Visita(fecha, diagnostico, precio));
+				}
+			}
+			con.store(d1);
+			con.commit();
+			return true;
+		} catch (ODBRuntimeException e) {
+			System.out.println("Error al intentar ejecutar la consulta.");
+			System.out.println("Detalle de error: " + e.getMessage());
+		} finally {
+			if (con != null)
+				try {
+					con.close();
+				} catch (ODBRuntimeException e2) {
+					System.err.println("Error al intentar cerrar la conexion. Detalle de error: " + e2.getMessage());
+				}
+		}
+		return false;
+	}
 }
